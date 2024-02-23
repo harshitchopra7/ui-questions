134c1741e4c6607a416d93ab8c12bb685dd73bc6
harshitchopra7 <chopraharshit2000@gmail.com>
2024-02-23 18:13:22 +0530

    traffic light

---
+++ src/Questions/TrafficLight/TrafficLight.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+
+// css
+import "./TrafficLight.css";
+
+// config
+import { config, LIGHTS } from "./constants";
+
+function TrafficLight() {
+  const [currentLight, setCurrentLight] = useState("red");
+
+  useEffect(() => {
+    scheduleTrafficLight(
+      config[LIGHTS.RED].timeDuration,
+      setCurrentLightAsYellow
+    );
+  }, []);
+
+  const scheduleTrafficLight = (time, cb) => {
+    return new Promise((resolve) => setTimeout(cb, time));
+  };
+
+  const setCurrentLightAsRed = async () => {
+    setCurrentLight(config[LIGHTS.RED].color);
+
+    scheduleTrafficLight(
+      config[LIGHTS.RED].timeDuration,
+      setCurrentLightAsYellow
+    );
+  };
+
+  const setCurrentLightAsYellow = async () => {
+    setCurrentLight(config[LIGHTS.YELLOW].color);
+    scheduleTrafficLight(
+      config[LIGHTS.YELLOW].timeDuration,
+      setCurrentLightAsGreen
+    );
+  };
+
+  const setCurrentLightAsGreen = async () => {
+    setCurrentLight(config[LIGHTS.GREEN].color);
+
+    scheduleTrafficLight(
+      config[LIGHTS.GREEN].timeDuration,
+      setCurrentLightAsRed
+    );
+  };
+
+  return (
+    <div className="traffic-light-alignment">
+      <div className="traffic-light-container">
+        <div
+          className={`traffic-lights ${
+            currentLight === LIGHTS.RED && config[LIGHTS.RED].color
+          }`}
+        ></div>
+        <div
+          className={`traffic-lights ${
+            currentLight === LIGHTS.YELLOW && config[LIGHTS.YELLOW].color
+          }`}
+        ></div>
+        <div
+          className={`traffic-lights ${
+            currentLight === LIGHTS.GREEN && config[LIGHTS.GREEN].color
+          }`}
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+export default TrafficLight;
---
+++ src/Questions/TrafficLight/constants.js
@@ -0,0 +1,20 @@
+export const LIGHTS = {
+  RED: "red",
+  YELLOW: "yellow",
+  GREEN: "green",
+};
+
+export const config = {
+  [LIGHTS.RED]: {
+    color: LIGHTS.RED,
+    timeDuration: 4000,
+  },
+  [LIGHTS.YELLOW]: {
+    color: LIGHTS.YELLOW,
+    timeDuration: 1000,
+  },
+  [LIGHTS.GREEN]: {
+    color: LIGHTS.GREEN,
+    timeDuration: 4000,
+  },
+};
